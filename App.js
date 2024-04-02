9e855fb52c87535287c2dcb8959b681ca01980be
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-04-02 18:40:43 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import StockChart from './StockChart';
+import StockChart from './StockChart'; // Asegúrate de que la ruta sea correcta
+import StockBarChart from './StockBarChart'; // Asegúrate de que la ruta sea correcta
+import PieChartComponent from './PieChartComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Gráfico de la Bolsa</h2>
-        <StockChart symbol="AAPL"/>
+        <h1>Visualización de Datos de Acciones</h1>
       </header>
+      <main>
+        {/* Gráfico de líneas para los precios de cierre */}
+        <section>
+          <h2>Gráfico de Líneas</h2>
+          <StockChart />
+        </section>
+
+        {/* Gráfico de barras para los precios de cierre */}
+        <section>
+          <h2>Gráfico de Barras</h2>
+          <StockBarChart />
+        </section>
+        <section>
+          <PieChartComponent />
+        </section>
+      </main>
     </div>
   );
 }
